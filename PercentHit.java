01dc529382129818e41037b3a385de622c43b5c8
Michael Liang <michael.liang@berkeley.edu>
2017-10-15 15:41:23 -0700

    Added random replacement

--- PercentHit.java
+++ PercentHit.java
@@ -8,6 +8,7 @@ public class PercentHit {
 		LRU lru_cache = new LRU(100);
 		MRU mru_cache = new MRU(100);
 		Clock clk = new Clock(100);
+        RandomR random = new RandomR(100);
 
 		for(int i = 0; i < 10000; i++) {
 			Integer rand = ThreadLocalRandom.current().nextInt(0, 1000); 
@@ -19,15 +20,20 @@ public class PercentHit {
             //System.out.println(mru_cache);
             clk.request(new ClockBuffer(rand));
             //System.out.println(clk);
-            System.out.println("lru: " + lru_cache.hitrate() + ",mru: " + mru_cache.hitrate() + ",clk: " + clk.hitrate());
+            random.insert(new Buffer(rand));
+            
 		}
+        System.out.println("lru: " + lru_cache.hitrate() + ",mru: " + mru_cache.hitrate()
+             + ",clk: " + clk.hitrate() + ",rand: " + random.hitrate());
         System.out.println(lru_cache.lst.size());
         System.out.println(mru_cache.lst.size());
         System.out.println(clk.lst.size());
+        System.out.println(random.lst.size());
 
         lru_cache = new LRU(500);
         mru_cache = new MRU(500);
         clk = new Clock(500);
+        random = new RandomR(500);
         for(int j = 0; j < 3; j++) { //sequential flooding
             for(int i = 0; i < 1000; i++) {
                 Integer rand = i; 
@@ -39,26 +45,14 @@ public class PercentHit {
                 //System.out.println(mru_cache);
                 clk.request(new ClockBuffer(rand));
                 //System.out.println(clk);
-                System.out.println("lru: " + lru_cache.hitrate() + ",mru: " + mru_cache.hitrate() + ",clk: " + clk.hitrate());
-            }//MRU  hits 1000 out of 3000, therefore 33%, everything else is a miss
+                random.insert(new Buffer(rand));
+               
+            }//MRU  hits 1000 out of 3000, therefore 33%
         }
+        System.out.println("lru: " + lru_cache.hitrate() + ",mru: " + mru_cache.hitrate()
+             + ",clk: " + clk.hitrate() + ",rand: " + random.hitrate());
 
-        try {
-            lru_cache = new LRU(0); //uh oh
-            mru_cache = new MRU(-1);
-            clk = new Clock(-134123);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e);
-            return;
-        }
-        
-
-        for(int i = 0; i < 10; i++) { //insert same value over and over and see what happens
-            lru_cache.insert(new Buffer(3.14));
-            mru_cache.insert(new Buffer(3.14));
-            clk.request(new ClockBuffer(3.14));
-        }
-        
+      
 	}
 
 
--- RandomR.java
+++ RandomR.java
@@ -6,6 +6,7 @@ public class RandomR {
     public List<Buffer> lst;
     public int pagehits; 
     public int totalhits; 
+    public int index;
     public RandomR(Integer size) throws IllegalArgumentException{
         if (size <= 0) {
             throw new IllegalArgumentException("Must initialize cache with positive number of entries.");
@@ -17,28 +18,64 @@ public class RandomR {
         }
         this.pagehits = 0;
         this.totalhits = 0;
+        index = 0;
     }
 
     public void insert(Buffer b) {
         totalhits++;
+        int nullIndex = -1;
         //if lst contains the buffer b
-        for(int i = 0; i < lst.size(); i++) { //TODO: could be condensed
+        for(int i = 0; i < lst.size(); i++) { //double for loop condensed with a null index...
             if (lst.get(i).equals(b)) {
                 pagehits++;
+                index = i;
                 return;
             }
-        }
-
-        //if lst contains null elements
-        for(int i = 0; i < lst.size(); i++) {
             if (lst.get(i).get_val() == null) {
-                lst.set(i, b);
-                return;
+                nullIndex = i;
             }
         }
 
+        if (nullIndex != -1) {
+            index = nullIndex;
+            lst.set(nullIndex, b);
+        } else {
+            int randomIndex = ThreadLocalRandom.current().nextInt(0, lst.size());
+            lst.set(randomIndex, b);
+            index = randomIndex;
+        }
         
+    }
 
+    public double hitrate() {
+        if (totalhits == 0) {
+            return 0;
+        }
+        return (double) pagehits / totalhits;
+    }
+
+    @Override
+    public String toString() {
+        String temp = "";
+        for(int i = 0; i < lst.size(); i++) {
+            if (i == index) {
+                temp += "[*" + lst.get(i).get_val() + "*]"; //can be misleading if your entry is all *****
+            } else {
+                temp += "[" + lst.get(i).get_val() + "]";
+            }
+        }
+        return temp + "\n";
+    }
+
+    public static void main(String[] args) {
+        RandomR random = new RandomR(10);
+        for(int j = 0; j < 2; j++) {
+            for(int i = 0; i < 15; i++) {
+                random.insert(new Buffer(i));
+                System.out.println(random);
+                System.out.println(random.hitrate());
+            }
+        }
         
     }
 }
