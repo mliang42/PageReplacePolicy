7c2f8d03cca6507b6f5b01ecf319e065a65d36dd
Michael Liang <michael.liang@berkeley.edu>
2017-10-29 18:52:26 -0700

    Having a hard time deciding on which implementation to use

--- ARC.java
+++ ARC.java
@@ -6,11 +6,13 @@ import java.util.concurrent.ThreadLocalRandom;
 public class ARC { //adaptive replacement cache
     public LRU T1;
     public LRU T2;
-    public LRU B1;
-    public LRU B2; //fixed of size c/2 where c is the size of the cache
+    public LRU B1; //ghost list
+    public LRU B2; //ghost list
     public int pagehits; 
     public int totalhits; 
-    public int index; //separator between L1 and L2 caches in lst
+
+    public int p; //maintains sizes between the 4 lsts
+    public int c; //cache size
 
 
     public ARC(Integer size) {
@@ -23,20 +25,42 @@ public class ARC { //adaptive replacement cache
         B2 = new LRU(size / 2); 
         pagehits = 0;
         totalhits = 0;
+        p = 0;
+        c = size;
     }
 
     public void insert(Buffer b) {
         int index_t1 = T1.checkIfContains(b);
         int index_t2 = T2.checkIfContains(b);
+        int index_b1 = B1.checkIfContains(b);
+        int index_b2 = B2.checkIfContains(b);
         //Case 1: b is in T1 or T2
         if (index_t1 != -1 || index_t2 != -1) {
             if (index_t1 != -1) { //it was a hit in T1
                 //remove entry from T1, update all LRU values
+                T1.remove(b); 
+                T2.insert(b);
             }
+        } else if (index_b1 != -1) { //ghost hit in B1
+            p = min(c, p + max(B1.size() / B2.size(), 1)); //adjustments toward p
+            replace(); 
+            B1.remove(b);
+            T2.insert(b);
+        } else if (index_b2 != -1) { //ghost hit in B2
+            p = max(0, p - max(B1.size() / B2, 1));
+            replace();
+            B2.remove(b);
+            T2.insert(b);
         }
         
 
     }
+
+    public void replace() {
+        
+    }
+
+
 
 
 
